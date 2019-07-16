fd599ecaf30adc23cd90b5789e79121e58e18c44
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-07-16 09:42:46 +0200

    Improve apps setup step
    Allow self configuration based on domain.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,43 @@
 import app from 'corteza-webapp-messaging/src/app'
+import router from './router'
 import './themes'
 import './plugins'
 
-app()
+window.localStorageKeys = {
+  domain: 'api.domain',
+  domains: 'api.domains',
+}
+
+// Initialize config
+const lsKeys = {
+  domain: { k: window.localStorageKeys.domain, d: null },
+  domains: { k: window.localStorageKeys.domains, d: [] },
+}
+
+// NOTE: domain object structure:
+// { domain: x, config: y, }
+
+// Check/fix localStorage's validity
+for (const { k, d } of Object.values(lsKeys)) {
+  if (!(k in localStorage)) {
+    localStorage.setItem(k, JSON.stringify(d))
+  }
+}
+
+// Check if domain is selected
+const domain = localStorage.getItem(lsKeys.domain.k)
+const domains = JSON.parse(localStorage.getItem(lsKeys.domains.k))
+const { config } = domains.filter(d => d.domain === domain).pop() || {}
+
+// If domain found & it's config is present, then use it; else use the base
+if (domain && config) {
+  Object.assign(window, config)
+} else {
+  window.configInvalid = true
+}
+
+// Run app
+document.addEventListener('deviceready', () => {
+  console.debug('device.ready')
+  app({ router })
+}, false)
