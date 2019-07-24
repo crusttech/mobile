e42e1f800c35cc970b4fe5d8861adaca96623547
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-07-24 12:11:40 +0200

    Add camera plugin

--- src/icons/fontawesome.js
+++ src/icons/fontawesome.js
@@ -1,9 +1,11 @@
-import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { faGlobe, faCamera, faImages } from '@fortawesome/free-solid-svg-icons'
 import faAuth from 'corteza-webapp-auth/src/icons/fontawesome'
 import faMessaging from 'corteza-webapp-messaging/src/icons/fontawesome'
 
 export default {
   faGlobe,
+  faCamera,
+  faImages,
   ...faAuth,
   ...faMessaging,
 }
