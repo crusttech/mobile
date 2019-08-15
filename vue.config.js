9d6ac785b7f00ebb2bff917709cf7302b19788ab
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-08-15 09:04:19 +0200

    Restructure vue config builder

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,24 @@
+const wpm = require('webpack-merge')
 const buildVueConfig = require('corteza-webapp-messaging/vue.config-builder')
 
-module.exports = buildVueConfig({
+const base = buildVueConfig({
   appName: 'messaging-hybrid',
   appLabel: 'Crust Messaging',
   theme: 'crust-hybrid-base',
   packageAlias: 'crust-hybrid-messaging',
+})
+
+module.exports = wpm(base, {
   publicPath: process.env.CORDOVA_PLATFORM ? '' : '/',
-  alias: {
-    vue$: `corteza-webapp-messaging/node_modules/vue/dist/vue.esm.js`,
-    i18next$: `corteza-webapp-messaging/node_modules/i18next`,
-  },
   pluginOptions: {
     cordovaPath: 'src-cordova',
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        vue$: `corteza-webapp-messaging/node_modules/vue/dist/vue.esm.js`,
+        i18next$: `corteza-webapp-messaging/node_modules/i18next`,
+      },
+    },
   },
 })
