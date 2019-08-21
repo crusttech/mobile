309c389452a765cb36b0fc0f37c4dd6f1fdcb104
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-08-21 18:22:24 +0200

    Remove aliases
    No longer required due to hoisting.

--- vue.config.js
+++ vue.config.js
@@ -13,12 +13,4 @@ module.exports = wpm(base, {
   pluginOptions: {
     cordovaPath: 'src-cordova',
   },
-  configureWebpack: {
-    resolve: {
-      alias: {
-        vue$: `corteza-webapp-messaging/node_modules/vue/dist/vue.esm.js`,
-        i18next$: `corteza-webapp-messaging/node_modules/i18next`,
-      },
-    },
-  },
 })
